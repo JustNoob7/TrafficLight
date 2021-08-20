72f4ff1c55dc862bbd1cf509b0bfc9f1d00c1339
Yaroslav Boyko <Spectraldagger322@gmail.com>
2021-08-20 15:53:46 +0300

    Deleted all previous. I have started to do it properly. The proper position of views in main.storyboard

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -16,33 +16,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redLightView.alpha = CGFloat(0.3)
-        yellowLightView.alpha = CGFloat(0.3)
-        greenLightView.alpha = CGFloat(0.3)
-        
-        
-        redLightView.layer.cornerRadius = 55
-        yellowLightView.layer.cornerRadius = 55
-        greenLightView.layer.cornerRadius = 55
-        startButton.layer.cornerRadius = 10
-    }
-
-    @IBAction func startButtonPressed() {
-        startButton.setTitle("Next", for: .normal)
-        
-        if redLightView.alpha != 1 && yellowLightView.alpha != 1 {
-            redLightView.alpha = 1
-            greenLightView.alpha = 0.3
-        } else if redLightView.alpha == 1 {
-            yellowLightView.alpha = 1
-            redLightView.alpha = 0.3
-        } else if yellowLightView.alpha == 1 {
-            greenLightView.alpha = 1
-            yellowLightView.alpha = 0.3
-        } else {
-            
-        }
-        
     }
     
 }
